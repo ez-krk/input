40d4abc05119a1edb513f4b35c9b2f7a636929b3
ez-krk <dev@krk.finance>
2023-09-14 19:40:16 +0200

    fixed z-index on title

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -12,7 +12,7 @@ export const HomeView: FC = () => {
 
   return (
     <div className="w-screen flex flex-col justify-center items-center">
-      <h1 className="leading-relaxed text-5xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195] tracking-widest">
+      <h1 className="leading-relaxed text-5xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195] tracking-widest z-[999]">
         <span className={marker.className}>input</span>
       </h1>
       <div className="text-lg text-white mb-3 leading-relaxed z-[750] ">
